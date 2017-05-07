2d1d0776aa0451938e8fd9b454ecb777a7787dd8
kimwah <kimwahwong@gmail.com>
2017-05-07 19:17:46 +0800

    Header done

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'; 
+import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
-    return (
-        <Provider store={createStore(reducers)}>
-            <View />
-        </Provider>
-    );
+  return (
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Tech Stack" />
+      </View>
+    </Provider>
+  );
 };
 
 export default App;
